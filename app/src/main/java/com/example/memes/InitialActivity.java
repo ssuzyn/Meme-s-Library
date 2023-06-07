91949f5fc5544ced46db36bcf5045e778df60c7a
ssuzyn <asj7090@gmail.com>
2023-06-07 17:32:14 +0900

    Feat: 앱 실행시 최초 화면 구현

---
+++ app/src/main/java/com/example/memes/InitialActivity.java
@@ -0,0 +1,31 @@
+package com.example.memes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnSignup = findViewById(R.id.btnSignup);
+        btnSignup.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+            startActivity(intent);
+        });
+    }
+
+
+}
